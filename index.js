37c4b691d00194d7cdc8416655a99bd2d207eddc
NoboniSultan <sultannoboni@gmail.com>
2025-10-01 14:24:07 -0400

    tip-02

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // Write a function that prints the first n square numbers and also calculates the sum of these numbers.
+let total = 0;
 const printSquareNumbers = (n) => {
-  //write code here
-
+  for (let i = 1; i <= n; i++) {
+    let sqr = i * i;
+    console.log(`Square of ${i} is ${sqr}`);
+    total += i * i; // Add the square of the current number to the total
+    console.log(`Total of all the squares are ${total}`);
+  }
 }
 printSquareNumbers(5);
 //will print: 1, 4, 9, 16, 25
